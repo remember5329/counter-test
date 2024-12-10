ffd23b2aea4b5b2946d2b511717db526dfbbbcca
dan <dan@conjecture.dev>
2024-12-10 14:21:38 +0000

    updated script

--- script.js
+++ script.js
@@ -2,37 +2,52 @@ let counter ;
 
 const incrementBy1 = async () => {
   // replace this with your own fetch by clicking "manage" after hovering over the incrementBy1 tactic in your workspace
-  const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
+  const response = await fetch("https://api.tactics.dev/api/workspace/1938d7db-1b0d-4a1f-963b-4db322cbdc60/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ initial_variables: {}, tactic_id: "b08cbcad-5331-4708-baa4-a8ad2f834d4a" }) ,
-  }) ;
-  const result = await response.json() ;
-  counter = result.result.content.value ;
+    body: JSON.stringify({ 
+      initial_variables: {}, // Add any initial variables you want to pass to the tactic
+      tactic_id: "d40404bf-79f7-41b4-85fd-1ce14b760f7c" 
+    })
+  });
+  
+  const data = await response.json();
+  const counter = JSON.parse(data.result.content.value) ;
+  
   document.getElementById('counterValue').textContent = counter ;
 } ;
 
 const incrementBy2 = async () => {
   // replace this with your own fetch by clicking "manage" after hovering over the incrementBy2 tactic in your workspace
-  const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
+  const response = await fetch("https://api.tactics.dev/api/workspace/1938d7db-1b0d-4a1f-963b-4db322cbdc60/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ initial_variables: {}, tactic_id: "7666d0c0-983f-4356-8818-38b397e98b6a" }) , 
-  }) ;
-  const result = await response.json() ;
-  counter = result.result.content.value ;
+    body: JSON.stringify({ 
+      initial_variables: {}, // Add any initial variables you want to pass to the tactic
+      tactic_id: "e3838283-fb7a-4620-9d0e-ee3cd2e2de17" 
+    })
+  });
+  
+  const data = await response.json();
+  const counter = JSON.parse(data.result.content.value) ;
+
   document.getElementById('counterValue').textContent = counter ;
 } ;
 
 const fetchCounter = async () => {
   // replace this with your own fetch by clicking "manage" after hovering over the fetchCounter tactic in your workspace
-  const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
+  const response = await fetch("https://api.tactics.dev/api/workspace/1938d7db-1b0d-4a1f-963b-4db322cbdc60/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ initial_variables: {}, tactic_id: "5b69b6ed-cc60-4c06-8b01-dcc47ec5eaf4" }) ,
-  }) ;
-  const result = await response.json() ;
-  counter = result.result.content.value ;
+    body: JSON.stringify({ 
+      initial_variables: {}, // Add any initial variables you want to pass to the tactic
+      tactic_id: "a9259df5-c27b-4a51-96b5-09f8840fdf4e" 
+    })
+  });
+
+  const data = await response.json();
+  const counter = JSON.parse(data.result.content.value) ;
+
   document.getElementById('counterValue').textContent = counter ;
 } ;
 
